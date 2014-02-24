0db2719de977d448de17e0095557ba8f1111f265
jiunjiun <quietmes@gmail.com>
2014-02-25 01:19:04 +0800

    First Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EatitWeb::Application.config.secret_key_base = '13f446ddd11fdeee802cf0ac33c15ba6a9a29d692501ccb217ba498c87d80af583bd648acbd1f75fa07fa94ce538a52e3cec2f07cf97ec62abb1e8684beaaeec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EatitWeb::Application.config.session_store :cookie_store, key: '_eatit_web_session'
