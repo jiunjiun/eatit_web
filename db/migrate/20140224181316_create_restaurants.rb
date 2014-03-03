b0f86e3520d98ca0ef5f35449a8eb18bd6d5e863
jiunjiun <quietmes@gmail.com>
2014-03-03 23:16:46 +0800

    增加兩個欄位

--- db/migrate/20140224181316_create_restaurants.rb
+++ db/migrate/20140224181316_create_restaurants.rb
@@ -1,12 +1,13 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-      t.string :category
-      t.string :name
-      t.string :address
-      t.string :telphone
-      t.float  :lat
-      t.float  :long
+      t.string  :category
+      t.string  :name
+      t.string  :address
+      t.string  :telephone
+      t.integer :count,    defualt: 0
+      t.float   :lat
+      t.float   :long
 
       t.timestamps
     end
